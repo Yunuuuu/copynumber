6abaddba886e4aeb18569a2dc8dc05cb2025fa2e
yun <yunyunpp96@outlook.com>
2023-04-23 15:46:19 +0800

    update rawdata prepare file

--- data-raw/add_assembly.R
+++ data-raw/add_assembly.R
@@ -1,18 +1,30 @@
 ## code to prepare `assembly` dataset goes here
 
-internal_data <- load("R/sysdata.rda")
-internal_data
-levels(hg19[["V1"]])
-if (!dir.exists("inst/extdata")) {
-    dir.create("inst/extdata")
-}
-for (i in internal_data) {
-    obj <- get(i, inherits = FALSE)
+# internal_data <- load("R/sysdata.rda")
+# if (!dir.exists("inst/extdata")) {
+#     dir.create("inst/extdata")
+# }
+# for (i in internal_data) {
+#     obj <- get(i, inherits = FALSE)
+#     saveRDS(obj, file = file.path("inst/extdata", paste0(i, ".rds")))
+# }
+
+# the first row of original mouse cytoband is regarded as colnames
+for (i in paste0("mm", 7:9)) {
+    obj <- data.table::fread(
+        sprintf("http://hgdownload.cse.ucsc.edu/goldenpath/%s/database/cytoBand.txt.gz", i),
+        header = FALSE
+    )
+    obj <- get_assembly(obj)
     saveRDS(obj, file = file.path("inst/extdata", paste0(i, ".rds")))
 }
 
-fct_cols <- names(hg19)[vapply(hg19, is.factor, logical(1L))]
 # add hg38 assembly -------------------------------------------------
+hg19 <- readRDS(system.file("extdata", paste0("hg19", ".rds"),
+    package = "copynumber"
+))
+levels(hg19[[1L]])
+fct_cols <- names(hg19)[vapply(hg19, is.factor, logical(1L))]
 if (!"hg38" %in% internal_data) {
     hg38 <- data.table::fread(
         "http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/cytoBand.txt.gz",
@@ -31,4 +43,3 @@ if (!"hg38" %in% internal_data) {
     }
     data.table::setDF(hg38)
 }
-
